3081961481c37a4ff6404aed70c59a91d61b6fe5
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-03-10 15:04:39 +0300

    refactor

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -7,30 +7,6 @@ interface Props {
   path: string;
 }
 
-/* const BackgroundImage = ({ imageUrl, slogan, path }: Props) => {
-  return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      <div className="absolute inset-0 bg-slate-900 opacity-60"></div>
-
-      <div className="relative text-center">
-        <h1 className="text-6xl font-bold font-serif text-white">{slogan}</h1>
-        <h3 className="text-wheat font-serif text-2xl mt-3 p-3">
-          <Link className="hover:text-yellow-400 transition-all" href="/">
-            Home
-          </Link>
-          {" / "}
-          {path}
-        </h3>
-      </div>
-    </div>
-  );
-};
-
-export default BackgroundImage; */
-
 const BackgroundImage = ({ imageUrl, slogan, path }: Props) => {
   return (
     <div
